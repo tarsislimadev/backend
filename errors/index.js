a5e37f3b30f5a4d11f3ec841373dc68437a56751
Tarsis <br.tmvdl@gmail.com>
2023-08-30 15:20:28 -0300

    Adding errors classes files

---
+++ errors/index.js
@@ -0,0 +1,39 @@
+
+class ApplicationError extends Error {
+  status = 400
+  message = 'Application Error'
+
+  constructor(status = this.status, message = this.message, extras = {}) {
+    super(message)
+    this.status = status
+    this.extras = extras
+  }
+
+  getStatus() {
+    return this.status
+  }
+
+  getMessage() {
+    return this.message
+  }
+
+  getExtras() {
+    return this.extras
+  }
+
+  get(key) {
+    return this.getExtras()[key]
+  }
+}
+
+class NotFoundError extends ApplicationError {
+  constructor(extras = {}) {
+    super(404, 'Not found.', extras)
+  }
+}
+
+module.exports = {
+  ApplicationError,
+  //
+  NotFoundError,
+}
