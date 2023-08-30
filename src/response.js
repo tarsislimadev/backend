5f0305aeb545d5c973ce3ac7236be6d0e938e2a0
Tarsis <br.tmvdl@gmail.com>
2023-08-30 15:22:07 -0300

    Updating response class file

--- src/response.js
+++ src/response.js
@@ -1,3 +1,39 @@
-class Response { }
+const { ApplicationError } = require('../errors/index.js')
+
+class Response {
+  status = 200
+  headers = new Headers()
+  body = ''
+
+  setJSON(json = {}, status = 200) {
+    this.status = status
+    this.headers.append('content-type', 'application/json')
+    this.body = JSON.stringify(json, null, 4)
+    return this
+  }
+
+  setError(error = new Error()) {
+    if (error instanceof ApplicationError) {
+      return this.setJSON(
+        {
+          status: 'error',
+          message: error.getMessage(),
+          data: error.getExtras(),
+        },
+        error.getStatus(),
+      )
+    }
+
+    return this.setJSON(
+      {
+        status: 'error',
+        message: error.message,
+        data: {},
+      },
+      500,
+    )
+  }
+
+}
 
 module.exports = { Response }
