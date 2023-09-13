8fdf2433cc4eb36297a9a7110fd664e2d3a5f98f
Tarsis <br.tmvdl@gmail.com>
2023-09-13 16:24:29 -0300

    Updating Cloudflare classes files

--- cloudflare/response.js
+++ cloudflare/response.js
@@ -2,6 +2,14 @@ import { Response as HttpResponse  } from '..'
 
 export class CloudflareResponse extends HttpResponse {
   getResponse() {
+    if (!(this.request.method == 'OPTIONS')) {
+      const status = 200
+      const headers = new Headers()
+      headers.append('Access-Control-Allow-Origin', '*')
+      headers.append('Access-Control-Allow-Headers', '*')
+      return new Response('', { headers, status })
+    }
+
     const { body, headers, status } = this
 
     return new Response(body, { headers, status })
