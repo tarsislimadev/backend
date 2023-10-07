9b3329ed8a6ee9f46b60d82bdac1e5c320edb9db
Tarsis <br.tmvdl@gmail.com>
2023-10-07 11:31:00 -0300

    Updating server files

--- src/server.js
+++ src/server.js
@@ -1,26 +1,29 @@
 import netPkg from 'net'
 import { HttpRequest } from './request.js'
 import { HttpResponse } from './response.js'
+import { Router } from './router.js'
 
 export class Server {
-  port = '80'
+  port = 80
 
-  requests = []
+  router = new Router()
 
   addRequest(method = 'GET', path = '/', fn = (() => { })) {
-    this.requests.push({ method, path, fn })
+    this.router.request(method, path, fn)
     return this
   }
 
   get(path = '/', fn = (() => { })) {
-    return this.addRequest({ method: 'GET', path, fn })
+    this.router.get(path, fn)
+    return this
   }
 
   post(path = '/', fn = (() => { })) {
-    return this.addRequest({ method: 'POST', path, fn })
+    this.router.post(path, fn)
+    return this
   }
 
-  setPort(port = '80') {
+  setPort(port = 80) {
     this.port = port
     return this
   }
@@ -29,15 +32,16 @@ export class Server {
     return res
   }
 
-  listen() {
+  listen(port = this.port) {
     const server = netPkg.createServer((socket) => {
       socket.on('data', (buffer) => {
-        const chunk = buffer.toString()
-        const req = new HttpRequest(chunk)
+        const req = new HttpRequest(buffer.toString())
         const res = new HttpResponse(req)
-        socket.write(this.run(req, res).toString())
+        socket.write(this.router.run(req, res).toString())
         socket.end()
       })
     })
+
+    server.listen(port, () => console.log(`listening on port ${port}`))
   }
 }
