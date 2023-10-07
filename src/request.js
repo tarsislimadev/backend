1bea12b898735423128e8764ae7f833975ba36c1
Tarsis <br.tmvdl@gmail.com>
2023-10-07 12:34:49 -0300

    Updating server files

--- src/request.js
+++ src/request.js
@@ -35,7 +35,7 @@ export class HttpRequest {
 
   parsePath(chunk) {
     const [headers,] = chunk.split(BREAK_LINE + BREAK_LINE)
-    const [first_line,] = headers
+    const [first_line,] = headers.split(BREAK_LINE)
     const [, fullpath,] = first_line.split(' ')
     const [path,] = fullpath.split('?', 2)
     return path
