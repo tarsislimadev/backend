95b558fc89e573083337a43709efd3a34cf166e8
Tarsis <br.tmvdl@gmail.com>
2023-10-27 11:58:11 -0300

    Updating CloudFlare files

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ export * from './request.js'
 export * from './response.js'
 export * from './router.js'
 export * from './server.js'
+// cloudflare
+export * from '../cloudflare/request.js'
+export * from '../cloudflare/response.js'
