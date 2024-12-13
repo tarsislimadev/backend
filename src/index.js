5c56c6e28f2ce0c4afa6f6e41c8461e735681baa
Tarsis Lima <br.tmvdl@gmail.com>
2024-12-13 11:55:59 -0300

    Updating app files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export * from './request.js'
+
 export * from './response.js'
+
 export * from './router.js'
+
 export * from './server.js'
-// cloudflare
-export * from '../cloudflare/request.js'
-export * from '../cloudflare/response.js'
