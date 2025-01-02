f20e794febbac8d0d13093f0f83cdee7d67ed115
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-02 17:00:40 -0300

    Updating app files

--- tests/index.js
+++ tests/index.js
@@ -6,4 +6,4 @@ const server = new Server()
 
 server.get('/', (req, res) => res.setJSON({ id: Date.now() }))
 
-server.listen(80)
+server.listen(80).then(() => fetch('http://localhost:80')).then((res) => res.json()).then((json) => console.log({ json }))
